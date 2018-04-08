9e156e3a4e9dda166d80322681086f2f8b4522ac
ottovbismarck <fritzj96@gmail.com>
2018-04-08 14:27:38 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+      mongoose = require('mongoose'),
+      logger = require('morgan'),
+      bcrypt = require('bcryptjs'),
+      passport = require('passport'),
+      bodyParser = require('body-parser'),
+      path = require('path'),
+      fs = require('fs'),
+      app = express();
+
+require('dotenv').config();
+
+app.set('env', process.env.NODE_ENV || 'development' );
+app.set('host', process.env.HOST || '0.0.0.0');
+app.set('port', process.env.PORT || 8080);
